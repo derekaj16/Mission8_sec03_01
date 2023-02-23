c8a440ba09c87845b0fc58ef47d324ec09044179
meldrumn <nashmeldrum@gmail.com>
2023-02-22 17:54:07 -0700

    nash edit

---
+++ Mission8_sec03_01/Models/ApplicationResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8_sec03_01.Models
+{
+    public class ApplicationResponse
+    {
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+        public string Task { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Quadrant { get; set; } 
+        public bool Completed { get; set; } 
+
+        // Build Foreign Key Relationship
+        public int CategoryID { get; set; } //dropdown
+        public Category Category { get; set; }
+    }
+}
